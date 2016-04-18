0b853cab76c89dd342d61c5ac3179652aef59483
Khovanov <vagrant@rails-dev-box>
2016-04-18 09:43:01 +0000

    create custom validation to Route

--- app/models/route.rb
+++ app/models/route.rb
@@ -4,4 +4,24 @@ class Route < ActiveRecord::Base
   has_many :trains 
   
   validates :name, presence: true
+  validate :stations_count
+
+  before_validation :set_name
+
+  private
+
+  def set_name
+    if railway_stations.present?
+      self.name = "#{railway_stations.first.title} - #{railway_stations.last.title}"
+    end
+  end
+
+  def stations_count
+    if railway_stations.size < 2
+      # для всего объекта:
+      # errors.add(:base, "Route should contain at least 2 stations")
+      # для поля объекта (label):
+      errors.add(:railway_station, "Route should contain at least 2 stations")
+    end
+  end
 end
