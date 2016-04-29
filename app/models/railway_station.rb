3a04fafdb2aebdfb9b0a81b6c88b7b7d2c40fc6e
Khovanov <vagrant@rails-dev-box>
2016-04-29 12:56:20 +0000

    add dependent destroy to RailwayStation and Route

--- app/models/railway_station.rb
+++ app/models/railway_station.rb
@@ -1,6 +1,6 @@
 class RailwayStation < ActiveRecord::Base
   has_many :trains
-  has_many :railway_stations_routes
+  has_many :railway_stations_routes, dependent: :destroy
   has_many :routes, through: :railway_stations_routes 
   
   validates :title, presence: true
